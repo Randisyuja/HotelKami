bc382eb7b8827ea6747182a17f8eaa23bb47f6e5
Randi Syuja <syuja131202@gmail.com>
2023-12-28 11:38:57 +0700

    change path

--- users/review.php
+++ users/review.php
@@ -64,7 +64,7 @@ if (isset($_POST["booknow"])){
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="../index.php">Home <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
